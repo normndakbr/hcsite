4211b71c9b076f0e14c40b1149aaab2580795664
ihf4n <53288192+ihf4n@users.noreply.github.com>
2023-07-06 08:22:09 +0800

    update_dashboard_2

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -3,7 +3,7 @@
 
 <head>
 
-     <title>eEmployee - Login</title>
+     <title>ESS - Login</title>
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -14,19 +14,31 @@
      <link rel="stylesheet" href="<?= base_url(); ?>assets/assets/css/style.css">
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-
+     <style>
+          .auth-wrapper {
+               background-image: url(<?= base_url('assets/assets/images/back/login.png') ?>);
+               background-size: cover;
+               background-repeat: no-repeat;
+          }
+     </style>
 </head>
 
 <div class="auth-wrapper">
-     <div class="auth-content">
-          <div class="card animate__animated animate__bounce">
+     <div class="auth-content" style="width:450px">
+          <div class="row">
+               <div class="col-lg-12 text-center">
+                    <h3 class="mb-3 f-w-400 text-white font-weight-bold">eEmployee Safety System</h3>
+                    <h4 class="mb-3 f-w-400 text-white font-weight-bold" style="margin-top: -10px;">Main System</h4>
+               </div>
+          </div>
+          <div class="card" style="border-radius: 2%;">
                <form action="<?= base_url('login/auth'); ?>" method="post">
                     <div class="row">
                          <div class="col-md-12">
                               <div class="card-body">
-                                   <div class="align-items-center text-center">
-                                        <h3 class="mb-3 f-w-400 font-weight-bold">eEmployee System</h3>
-                                        <h4 class="mb-3 f-w-400">Selamat Datang</h4>
+                                   <div class="align-items-center text-center" style="margin-top: -35px;">
+                                        <img src="<?= base_url('assets/assets/images/auth/idc.jpg'); ?>" alt="PT Indexim Coalindo" class="w-75 mt-1">
+                                        <h4 class="text-black font-weight-bolder mt-2">PT INDEXIM COALINDO</h4>
                                         <?= $this->session->userdata('pesan'); ?>
                                    </div>
                                    <div class="form-group mb-3">
@@ -39,12 +51,7 @@
                                         <input type="password" class="form-control" id="tsandi" name="tsandi" placeholder="">
                                         <?= form_error('tsandi', '<small class="text-danger font-italic font-weight-bold">', ' </small>') ?>
                                    </div>
-                                   <div class="custom-control custom-checkbox text-left mb-4 mt-2">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Ingatkan saya</label>
-                                   </div>
                                    <button type='submit' class="btn btn-block btn-primary mb-4 uiusdd123">Masuk</button>
-                                   <p class="mb-2 text-muted">Lupa Sandi? <a href="#" class="f-w-400">Reset</a></p>
                               </div>
                          </div>
                     </div>
